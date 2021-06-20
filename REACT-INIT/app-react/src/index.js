07e88338867762f0419edec4df63b8e43977d306
Robson Inocêncio <robson.inocencio@gmail.com>
2021-06-20 04:56:24 -0300

    Primeiro componente e conceitos por trás.

--- REACT-INIT/app-react/src/index.js
+++ REACT-INIT/app-react/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
