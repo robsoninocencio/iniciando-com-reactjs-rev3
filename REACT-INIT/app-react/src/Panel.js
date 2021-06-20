514583713f93411e6c159274c577db69a299ea9f
Robson Inocêncio <robson.inocencio@gmail.com>
2021-06-20 17:02:06 -0300

    Praticando props.

---
+++ REACT-INIT/app-react/src/Panel.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class Panel extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { headercolor, bordercolor, headertext, data } = this.props;
+        return (
+            <div className="panel" style={{ border: `1px solid ${bordercolor}`, width: '500px', height: '100px', position: 'relative' }}>
+                <div className="panel-head" style={{ width: '100%', height: '50px', background: headercolor }}>
+                    {headertext}
+                </div>
+                <div className="panel-body">
+                    {data}
+                </div>
+            </div>
+        );
+    }
+}
--- REACT-INIT/app-react/src/Props.js
+++ REACT-INIT/app-react/src/Props.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Panel from './Panel';
 
 export default class Props extends Component {
     constructor(props) {
@@ -13,6 +14,8 @@ export default class Props extends Component {
             <div>
                 {attr === 'p' ? <p>props.attr = {attr}</p> : <h1>props.attr = {attr}</h1>}
                 {attr2 === 'p' ? <p>props.attr2 = {attr2}</p> : <h1>props.attr2 = {attr2}</h1>}
+                <Panel headercolor="red" bordercolor="blue" headertext="This is a header" data="School of net" />
+                <Panel headercolor="gray" bordercolor="purple" headertext="This is a header" data="School of net" />
             </div>
         );
     }
